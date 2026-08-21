e5198bf5d69627dd0df656281ac42ef3e99d9483
Diksha Somvanshi <somvanshidiksha5@gmail.com>
2026-08-21 01:24:11 +0000

    Add variables basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12221
+let email = "somvanshidiksha@gmail.com"
+var accountPassword = "12345"
+accountcity = "jaipur"
+
+// accountId = 2;-->assignment to constant variable
+
+email = "dii2123"
+accountPassword = "121111"
+// accountcity="Bengaluru"-->this should not be used
+console.table([accountId,accountPassword,email])

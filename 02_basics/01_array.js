6027e4f10da2ffa56fbad3d270c4f212d4e71099
Diksha Somvanshi <somvanshidiksha5@gmail.com>
2026-08-23 18:39:31 +0530

    Add documentation for JavaScript arrays
    Added comments explaining JavaScript arrays, including their properties, copy operations, and methods.

---
+++ 02_basics/01_array.js
@@ -0,0 +1,26 @@
+// array is object
+//collection of multiple elements
+//js arrays are resizable
+
+// mixed datatype values can be stored in same array
+//0 based indexing
+
+//array copy operation makes shallow copy 
+//shallow copy --> shares the same reference of array
+//changes made in copy reflects in original array 
+
+
+//deep copy_---> do not share the same reference
+//changes made in copy do not affect original array
+
+//Array methods
+
+const myArr = [0, 1, 2, 3, 4, 5];
+console.log(typeof myArr)
+const heros = ["shakti","naagraj"];
+
+const arr2 = new Array(1,2,2,3);
+console.log(arr2)
+
+console.log(myArr[0]);
+console.log(myArr.length);

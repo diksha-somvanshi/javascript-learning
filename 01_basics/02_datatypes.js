35af57020a20c4649128168935e3f631939ccce8
Diksha Somvanshi <somvanshidiksha5@gmail.com>
2026-08-23 12:53:15 +0530

    Add files via upload

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3)// we areusing nodejs, not browser
+
+console.log(3+3)
+
+let name="diksha"
+let age = 13;
+let isloggedIn = false
+
+//number=>2 to power 53
+//bigint - high numbers used in stock market
+//String = ""
+//boolean = true/false
+//null - standalone value --> empty
+//undefined - not defined i.e value is not assigned
+//symbol - uniqueness
+
+//object 
+
+console.log(typeof name)
+console.log(typeof 5)
+console.log(typeof null)
+console.log(typeof undefined)
